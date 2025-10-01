ba27213893ad43fbcde7a1a58e22fbf35740929d
prywatny <prywatny@Michals-MacBook-Pro.local>
2025-10-01 19:23:06 +0200

    created base for app

---
+++ src/app/app/app.ts
@@ -0,0 +1,14 @@
+import { Component, signal } from '@angular/core';
+import { Header } from '../header/header';
+import { User } from "../user/user";
+import { DUMMY_USERS } from '../dummy-users';
+
+@Component({
+  selector: 'app-root',
+  imports: [Header, User],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  users = DUMMY_USERS
+}
---
+++ src/app/user/user.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.html',
+  styleUrl: './user.scss'
+})
+export class User {
+  @Input() avatar!: string;
+  @Input() name!: string;
+
+  get imagePath(){
+    return 'assets/users/' + this.avatar
+  }
+
+  onSelectUser(){}
+}
